e838dd98410ed5cb8486ab321cf2a3f0e7d5c0fe
ramakrish5149 <krishevolve@gmail.com>
2024-07-13 18:37:34 +0100

    Create ArrayOddPosition.java

---
+++ ArrayOddPosition.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class ArrayOddPosition{
+  public static void main(String[] args){
+    int[] arr = new int[] {7,4,9,3,8,,37,90};
+    System.out.print("Elements of given array in odd position: ");
+    for(int i=0;i<arr.length;i++){
+      System.out.print(arr[i]);
+    }
+  }
+}
+    
