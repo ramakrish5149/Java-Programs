ec510f5634d38858012fbdb6de447c07b76a4a75
ramakrish5149 <krishevolve@gmail.com>
2024-07-14 15:44:56 +0100

    Create StringEquals.java

---
+++ StringEquals.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class StringEquals{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    String s1=sc.nextLine();
+    String s2=sc.nextLine();
+    if(str1==str2){
+      System.out.println("Equal");
+    }
+    else{
+      System.out.println("NOT Equal");
+    }
+  }
+}
