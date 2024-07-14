c3f20512dea3e274ee8dd5cd8aaa6ddd80cb11e1
ramakrish5149 <krishevolve@gmail.com>
2024-07-14 15:16:05 +0100

    Create PowerOfNumber.java\

---
+++ PowerOfNumber.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class PowerOfNumber{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter base number");
+    int a=sc.nextInt();
+    System.out.println("Enter power number");
+    int b=sc.nextInt();
+    int result=1;
+    while(n>0){
+      result*=a;
+      --b;
+   }
+    System.out.println("Result "+result);
+  }
+}
